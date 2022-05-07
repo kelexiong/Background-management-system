c7bb3eb66d77be3913de9fefe1833fafb78e199a
kele <984739634@qq.com>
2022-05-07 21:55:31 +0800

    头天home外观样式

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HeardSeac></HeardSeac>
+    <FooterDetails></FooterDetails>
+  </div>
+</template>
+
+<script>
+import HeardSeac from './components/head/HeardSeac.vue'
+import FooterDetails from './components/footer/FooterDetails.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeardSeac,
+    FooterDetails
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
